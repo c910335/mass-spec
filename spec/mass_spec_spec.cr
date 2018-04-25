5ecea64a6dd52c751ea650ea6701273965256fb5
Tatsiujin Chin <c910335@gmail.com>
2018-04-25 19:19:41 +0800

    add simple spec

--- spec/mass_spec_spec.cr
+++ spec/mass_spec_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
 
-describe MassSpec do
-  # TODO: Write tests
+server = HTTP::Server.new(8080) do |context|
+  context.response.content_type = "application/json"
+  context.response.print({path: context.request.path}.to_json)
+end
 
-  it "works" do
-    false.should eq(true)
+server.listen
+
+describe "Server" do
+  it "returns the path in json" do
+    get("/nas/beru/uhn'adarr")
+    body.should eq(%({"path":"/nas/beru/uhn'adarr"}))
+    json_body.should eq({"path" => "/nas/beru/uhn'adarr"})
   end
 end
