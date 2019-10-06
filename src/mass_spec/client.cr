368ae2f261f3c54507d86ec01f8e3784643d1232
Tatsiujin Chin <c910335@gmail.com>
2019-10-07 06:44:26 +0800

    compatible with webmock

--- src/mass_spec/client.cr
+++ src/mass_spec/client.cr
@@ -1,10 +1,10 @@
 class MassSpec::Client < HTTP::Client
-  private def exec_internal_single(request)
+  private def exec_internal(request)
     decompress = set_defaults request
     input = IO::Memory.new
     request.to_io(input)
     input.rewind
     output = MassSpec.server.call(input)
-    HTTP::Client::Response.from_io?(output, ignore_body: request.ignore_body?, decompress: decompress)
+    HTTP::Client::Response.from_io(output, ignore_body: request.ignore_body?, decompress: decompress)
   end
 end
